6f955cda0366f5190e480235f42f0ac2d961b457
EddyGlezz <eddyglezz95@gmail.com>
2022-12-15 14:17:18 +0100

    fixed the finsodium for wasm

--- CMake/FindSodium.cmake
+++ CMake/FindSodium.cmake
@@ -78,13 +78,13 @@ if (UNIX)
     endif()
 
     find_path(sodium_INCLUDE_DIR sodium.h
-        HINTS ${${XPREFIX}_INCLUDE_DIRS}
+        HINTS ${${XPREFIX}_INCLUDE_DIRS} NO_CMAKE_FIND_ROOT_PATH
     )
     find_library(sodium_LIBRARY_DEBUG NAMES ${${XPREFIX}_LIBRARIES}
-        HINTS ${${XPREFIX}_LIBRARY_DIRS}
+        HINTS ${${XPREFIX}_LIBRARY_DIRS} NO_CMAKE_FIND_ROOT_PATH
     )
     find_library(sodium_LIBRARY_RELEASE NAMES ${${XPREFIX}_LIBRARIES}
-        HINTS ${${XPREFIX}_LIBRARY_DIRS}
+        HINTS ${${XPREFIX}_LIBRARY_DIRS} NO_CMAKE_FIND_ROOT_PATH
     )
 
 
