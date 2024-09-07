59f0b4fabd622be21afc95647d8539ca4cf782a4
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-09-07 12:08:38 +0200

    align names of namespaces, targets ...

--- include/esterv/crypto/ed25519.hpp
+++ include/esterv/crypto/ed25519.hpp
@@ -2,14 +2,10 @@
 
 #include <QByteArray>
 
-namespace qcrypto
+namespace Esterv:Crypto:Ed25519
 {
-namespace qed25519
-{
-
 std::pair<QByteArray, QByteArray> create_keypair(const QByteArray &seed);
 QByteArray sign(const std::pair<QByteArray, QByteArray> &key_pair, const QByteArray &message);
 bool verify(const QByteArray &signature, const QByteArray &message, const QByteArray &public_key);
-} // namespace qed25519
 
 }; // namespace qcrypto
